4e63ece79114832e675d7c1e3e986190bab81cd3
Fearium <jessebaril.programmer@gmail.com>
2016-06-08 22:41:48 -0400

    Add project files.

---
+++ Meteor/StudentDetails.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Using statements required for EF DB acess
+using Meteor.Models;
+using System.Web.ModelBinding;
+
+namespace Meteor
+{
+    public partial class StudentDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Redirect back to Students Page
+            Response.Redirect("~/Students.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the server
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // Use the Student model to create a new student object and save a new record
+                Student newStudent = new Student();
+
+                // Add data to the new student record
+                newStudent.LastName = LastNameTextBox.Text;
+                newStudent.FirstMidName = FirstNameTextBox.Text;
+                newStudent.EnrollmentDate = Convert.ToDateTime(EnrollmentDateTextBox.Text);
+
+                // Use LINQ to ADO.NET to add or insert new student into the database
+                db.Students.Add(newStudent);
+
+                // Save our changes
+                db.SaveChanges();
+
+                // Redirect back to the updated students page
+                Response.Redirect("~/Students.aspx");
+            }
+        }
+    }
+}
---
+++ Meteor/Students.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Using statements that are required to connect to EF DB
+using Meteor.Models;
+using System.Web.ModelBinding;
+
+namespace Meteor
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // If loading the page for the first time, populate the student grid
+            if (!IsPostBack)
+            {
+                // Get the student data
+                this.GetStudents();
+            }
+        }
+
+        /**
+         * <summary>
+         * This method gets the student data from the DB
+         * </summary>
+         * 
+         * @method GetStudents
+         * @returns {void}
+         */
+
+        protected void GetStudents()
+        {
+            // Connect to EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // Query the Students Table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                // Bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+        }
+    }
+}
